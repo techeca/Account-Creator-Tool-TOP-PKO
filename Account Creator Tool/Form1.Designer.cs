05bd986e83468dcd63554c576c737872604ea718
techeca <jp.vasquez@outlook.com>
2022-05-20 18:08:43 -0400

    Agregar archivos de proyecto.

---
+++ Account Creator Tool/Form1.Designer.cs
@@ -0,0 +1,292 @@
+﻿
+namespace Account_Creator_Tool
+{
+    partial class accountCreator
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtGameName = new System.Windows.Forms.TextBox();
+            this.txtAccountName = new System.Windows.Forms.TextBox();
+            this.btnDisconnect = new System.Windows.Forms.Button();
+            this.lblInfoDB = new System.Windows.Forms.Label();
+            this.txtIPDB = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtPasswordDB = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtUserDB = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.chBGM = new System.Windows.Forms.CheckBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.btnInsertUser = new System.Windows.Forms.Button();
+            this.txtUserPassword = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtUser = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtGameName);
+            this.groupBox1.Controls.Add(this.txtAccountName);
+            this.groupBox1.Controls.Add(this.btnDisconnect);
+            this.groupBox1.Controls.Add(this.lblInfoDB);
+            this.groupBox1.Controls.Add(this.txtIPDB);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.txtPasswordDB);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txtUserDB);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.btnConnect);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(303, 159);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Database";
+            // 
+            // txtGameName
+            // 
+            this.txtGameName.Location = new System.Drawing.Point(194, 61);
+            this.txtGameName.Name = "txtGameName";
+            this.txtGameName.Size = new System.Drawing.Size(91, 23);
+            this.txtGameName.TabIndex = 10;
+            this.txtGameName.MouseEnter += new System.EventHandler(this.txtGameName_MouseEnter);
+            this.txtGameName.MouseLeave += new System.EventHandler(this.txtGameName_MouseLeave);
+            // 
+            // txtAccountName
+            // 
+            this.txtAccountName.Location = new System.Drawing.Point(195, 32);
+            this.txtAccountName.Name = "txtAccountName";
+            this.txtAccountName.Size = new System.Drawing.Size(90, 23);
+            this.txtAccountName.TabIndex = 9;
+            this.txtAccountName.MouseEnter += new System.EventHandler(this.txtAccountName_MouseEnter);
+            this.txtAccountName.MouseLeave += new System.EventHandler(this.txtAccountName_MouseLeave);
+            // 
+            // btnDisconnect
+            // 
+            this.btnDisconnect.Location = new System.Drawing.Point(159, 122);
+            this.btnDisconnect.Name = "btnDisconnect";
+            this.btnDisconnect.Size = new System.Drawing.Size(75, 23);
+            this.btnDisconnect.TabIndex = 8;
+            this.btnDisconnect.Text = "Disconnect";
+            this.btnDisconnect.UseVisualStyleBackColor = true;
+            this.btnDisconnect.Click += new System.EventHandler(this.btnDisconnect_Click);
+            // 
+            // lblInfoDB
+            // 
+            this.lblInfoDB.AutoSize = true;
+            this.lblInfoDB.Location = new System.Drawing.Point(27, 126);
+            this.lblInfoDB.Name = "lblInfoDB";
+            this.lblInfoDB.Size = new System.Drawing.Size(34, 15);
+            this.lblInfoDB.TabIndex = 7;
+            this.lblInfoDB.Text = "INFO";
+            // 
+            // txtIPDB
+            // 
+            this.txtIPDB.Location = new System.Drawing.Point(88, 32);
+            this.txtIPDB.Name = "txtIPDB";
+            this.txtIPDB.Size = new System.Drawing.Size(101, 23);
+            this.txtIPDB.TabIndex = 6;
+            this.txtIPDB.TextChanged += new System.EventHandler(this.txtIPDB_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(15, 39);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 15);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "IP/NameDB";
+            // 
+            // txtPasswordDB
+            // 
+            this.txtPasswordDB.Location = new System.Drawing.Point(88, 90);
+            this.txtPasswordDB.Name = "txtPasswordDB";
+            this.txtPasswordDB.Size = new System.Drawing.Size(100, 23);
+            this.txtPasswordDB.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 98);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Password";
+            // 
+            // txtUserDB
+            // 
+            this.txtUserDB.Location = new System.Drawing.Point(88, 61);
+            this.txtUserDB.Name = "txtUserDB";
+            this.txtUserDB.Size = new System.Drawing.Size(100, 23);
+            this.txtUserDB.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 68);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "User DB";
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(78, 122);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(75, 23);
+            this.btnConnect.TabIndex = 0;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.chBGM);
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Controls.Add(this.btnInsertUser);
+            this.groupBox2.Controls.Add(this.txtUserPassword);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.txtUser);
+            this.groupBox2.Location = new System.Drawing.Point(321, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(221, 159);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "New Account";
+            // 
+            // chBGM
+            // 
+            this.chBGM.Location = new System.Drawing.Point(21, 92);
+            this.chBGM.Name = "chBGM";
+            this.chBGM.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.chBGM.Size = new System.Drawing.Size(126, 19);
+            this.chBGM.TabIndex = 6;
+            this.chBGM.Text = "GM";
+            this.chBGM.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.chBGM.UseVisualStyleBackColor = true;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(21, 125);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(34, 15);
+            this.label7.TabIndex = 5;
+            this.label7.Text = "INFO";
+            // 
+            // btnInsertUser
+            // 
+            this.btnInsertUser.Location = new System.Drawing.Point(115, 121);
+            this.btnInsertUser.Name = "btnInsertUser";
+            this.btnInsertUser.Size = new System.Drawing.Size(75, 23);
+            this.btnInsertUser.TabIndex = 4;
+            this.btnInsertUser.Text = "Insert User";
+            this.btnInsertUser.UseVisualStyleBackColor = true;
+            this.btnInsertUser.Click += new System.EventHandler(this.btnInsertUser_Click);
+            // 
+            // txtUserPassword
+            // 
+            this.txtUserPassword.Location = new System.Drawing.Point(90, 60);
+            this.txtUserPassword.Name = "txtUserPassword";
+            this.txtUserPassword.Size = new System.Drawing.Size(100, 23);
+            this.txtUserPassword.TabIndex = 3;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(21, 63);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(57, 15);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "Password";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(21, 38);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(30, 15);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "User";
+            // 
+            // txtUser
+            // 
+            this.txtUser.Location = new System.Drawing.Point(90, 31);
+            this.txtUser.Name = "txtUser";
+            this.txtUser.Size = new System.Drawing.Size(100, 23);
+            this.txtUser.TabIndex = 0;
+            // 
+            // accountCreator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(563, 186);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "accountCreator";
+            this.Text = "Account Creator";
+            this.Load += new System.EventHandler(this.accountCreator_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label lblInfoDB;
+        private System.Windows.Forms.TextBox txtIPDB;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtPasswordDB;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtUserDB;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button btnInsertUser;
+        private System.Windows.Forms.TextBox txtUserPassword;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtUser;
+        private System.Windows.Forms.Button btnDisconnect;
+        private System.Windows.Forms.TextBox txtAccountName;
+        private System.Windows.Forms.CheckBox chBGM;
+        private System.Windows.Forms.TextBox txtGameName;
+    }
+}
+
---
+++ Account Creator Tool/Form1.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Security.Cryptography;
+
+namespace Account_Creator_Tool
+{
+    public partial class accountCreator : Form
+    {
+
+        string StringConn = string.Empty;
+        string StringConn2 = string.Empty;
+        SqlConnection conn = null;
+        SqlConnection conn2 = null;
+        SqlCommand mySqlCommand = null;
+
+        public accountCreator()
+        {
+            InitializeComponent();
+        }
+
+        private void accountCreator_Load(object sender, EventArgs e)
+        {
+            btnDisconnect.Enabled = false;
+            txtAccountName.Text = "AccountServer";
+            txtAccountName.ForeColor = Color.Gray;
+            txtGameName.Text = "GameDB";
+            txtGameName.ForeColor = Color.Gray;
+            txtIPDB.Text = "IP Server";
+            txtIPDB.ForeColor = Color.Gray;
+        }
+
+        
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            if(txtIPDB.Text != "" && txtAccountName.Text != "" && txtUserDB.Text != "")
+            {
+                string serverDat = txtIPDB.Text;
+                string databaseDat = txtAccountName.Text;
+                string databaseDat2 = txtGameName.Text;
+                string userDat = txtUserDB.Text;
+                string passwordDat = txtPasswordDB.Text;
+
+                try
+                {
+                    StringConn = @"Server=" + serverDat + ";Database=" + databaseDat + ";User Id=" + userDat + ";Password=" + passwordDat;
+                    StringConn2 = @"Server=" + serverDat + ";Database=" + databaseDat2 + ";User Id=" + userDat + ";Password=" + passwordDat;
+                    conn = new SqlConnection(StringConn);
+                    conn2 = new SqlConnection(StringConn2);
+                    //conn.Open();
+                    lblInfoDB.Text = "ON";
+                    lblInfoDB.ForeColor = Color.Green;
+                    btnDisconnect.Enabled = true;
+                    btnConnect.Enabled = false;
+                    txtIPDB.Enabled = false;
+                    txtAccountName.Enabled = false;
+                    txtUserDB.Enabled = false;
+                    txtPasswordDB.Enabled = false;
+                }
+                catch (SqlException ex)
+                {
+                    lblInfoDB.Text = "ERROR";
+                    lblInfoDB.ForeColor = Color.Red;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Empty fields, please fill IP, DBName and User.");
+            }
+
+            
+
+        }
+        private void btnDisconnect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                conn.Close();
+                conn2.Close();
+                lblInfoDB.Text = "OFF";
+                lblInfoDB.ForeColor = Color.Red;
+                btnConnect.Enabled = true;
+                btnDisconnect.Enabled = false;
+                txtIPDB.Enabled = true;
+                txtAccountName.Enabled = true;
+                txtUserDB.Enabled = true;
+                txtPasswordDB.Enabled = true;
+            }
+            catch (SqlException ex)
+            {
+                lblInfoDB.Text = "ERROR";
+                lblInfoDB.ForeColor = Color.Red;
+            }
+        }
+
+        private void btnInsertUser_Click(object sender, EventArgs e)
+        {
+            string nameUser = txtUser.Text;
+            string passUser = txtUserPassword.Text;
+
+            try
+            {
+                if(chBGM.Checked == true)
+                {
+
+                    conn.Open();
+                    string sqlQuery = "INSERT INTO account_login (name, password) VALUES (@Name, @Password)";
+                    mySqlCommand = new SqlCommand(sqlQuery, conn);
+                    mySqlCommand.Parameters.AddWithValue("Name", nameUser);
+                    mySqlCommand.Parameters.AddWithValue("Password", MD5Hash(passUser).ToUpper());
+                    mySqlCommand.ExecuteNonQuery();
+                    conn.Close();
+                    string sqlQuery2 = "INSERT INTO account (act_id, gm, act_name) VALUES ((SELECT MAX(act_id) + 1 FROM account), 99, @Act_name)";
+                    conn2.Open();
+                    mySqlCommand = new SqlCommand(sqlQuery2, conn2);
+                    mySqlCommand.Parameters.AddWithValue("Act_name", nameUser);
+                    mySqlCommand.ExecuteNonQuery();
+                    conn2.Close();
+                }
+                else
+                {
+                    conn.Open();
+                    string sqlQuery = "INSERT INTO account_login (name, password) VALUES (@Name, @Password)";
+                    mySqlCommand = new SqlCommand(sqlQuery, conn);
+                    mySqlCommand.Parameters.AddWithValue("Name", nameUser);
+                    mySqlCommand.Parameters.AddWithValue("Password", MD5Hash(passUser).ToUpper());
+                    mySqlCommand.ExecuteNonQuery();
+                    conn.Close();
+
+                    string sqlQuery2 = "INSERT INTO account (act_id, act_name) VALUES ((SELECT MAX(act_id) + 1 FROM account), @Act_name)";
+                    conn2.Open();
+                    mySqlCommand = new SqlCommand(sqlQuery2, conn2);
+                    mySqlCommand.Parameters.AddWithValue("Act_name", nameUser);
+                    mySqlCommand.ExecuteNonQuery();
+                    conn2.Close();
+                }
+
+                //sqlQuery2 = "INSERT INTO account (act_id, act_name) VALUES ((SELECT MAX(act_id) + 1 FROM account), @Act_name)";
+                
+
+                MessageBox.Show("New user added!!, USER: " + nameUser + ", PASS: " + MD5Hash(passUser));
+                txtUser.Text = "";
+                txtUserPassword.Text = "";
+
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        public static string MD5Hash(string input)
+        {
+            StringBuilder hash = new StringBuilder();
+            MD5CryptoServiceProvider md5Provider = new MD5CryptoServiceProvider();
+            byte[] bytes = md5Provider.ComputeHash(new UTF8Encoding().GetBytes(input));
+
+            for (int i = 0; i < bytes.Length; i++)
+            {
+                hash.Append(bytes[i].ToString("x2"));
+            }
+
+            return hash.ToString();
+        }
+
+        private void txtAccountName_MouseEnter(object sender, EventArgs e)
+        {
+            if(txtAccountName.Text == "AccountServer")
+            {
+                txtAccountName.Text = "";
+            }
+        }
+
+        private void txtAccountName_MouseLeave(object sender, EventArgs e)
+        {
+            if(txtAccountName.Text == "")
+            {
+                txtAccountName.Text = "AccountServer";
+            }
+        }
+
+        private void txtGameName_MouseEnter(object sender, EventArgs e)
+        {
+            if(txtGameName.Text == "GameDB")
+            {
+                txtGameName.Text = "";
+            }
+        }
+
+        private void txtGameName_MouseLeave(object sender, EventArgs e)
+        {
+            if(txtGameName.Text == "")
+            {
+                txtGameName.Text = "GameDB";
+            }
+        }
+
+        private void txtIPDB_TextChanged(object sender, EventArgs e)
+        {
+            if(txtIPDB.Text == "")
+            {
+                txtIPDB.Text = "IP SERVER";
+            }
+        }
+    }
+}
